32eaadeb9e7c50f8ae23fa6b45a83fbbe8240a03
Abhilash <abhilashlr.789@gmail.com>
2019-11-28 19:12:51 +0530

    Animations for list view

--- app/src/component/ListItem.js
+++ app/src/component/ListItem.js
@@ -1,14 +1,6 @@
-import React, {Component, Fragment} from 'react';
-import styled, { keyframes } from 'styled-components';
-import { bounce } from 'react-animations';
-import APP from '../services/client';
+import React, { Component, Fragment } from 'react';
+// import APP from '../services/client';
 
-// const animatedProps = useSpring({opacity: 1, from: {opacity: 0}});
-const bounceAnimation = keyframes`${bounce}`;
-
-const BouncyDiv = styled.div`
-  animation: 1s ${bounceAnimation};
-`;
 class ListItem extends Component {
   constructor(props) {
     super(props);
@@ -18,14 +10,10 @@ class ListItem extends Component {
   }
 
   render() {
-    APP.client.db.get(`${APP.currentUser.id}`).then(data => {
-      // console.log(data);
-      // [TODO]: Use this to render the list of recents used.
-    });
-
-    console.log(BouncyDiv);
-
-    const {listItemView} = this.state;
+    // APP.client.db.get(`${APP.currentUser.id}`).then(data => {
+    //   // console.log(data);
+    //   // [TODO]: Use this to render the list of recents used.
+    // });
 
     const toggleListItem = () => {
       this.setState(prevState => ({
@@ -42,15 +30,14 @@ class ListItem extends Component {
             <img alt="List Item" src="https://img.icons8.com/ios-glyphs/50/000000/menu.png" />
           </button>
         {/* </div> */}
-        {listItemView ?
-          <BouncyDiv className="top-items-absolute-container">
-            <div className="top-items-relative-container">
-              <div className="top-items-container-header">Top asks</div>
-              <div className="top-items-container-list">Freddy the robot</div>
-              <div className="top-items-container-list">Kamal the dirty</div>
-              <div className="top-items-container-list">Monday syed</div>
-            </div>
-          </BouncyDiv> : null}
+        <div className={`animated faster top-items-absolute-container ${this.state.listItemView ? 'slideInUp' : 'slideOutDown fadeOut'}`}>
+          <div className="top-items-relative-container">
+            <div className="top-items-container-header">Top asks</div>
+            <div className="top-items-container-list">Freddy the robot</div>
+            <div className="top-items-container-list">Kamal the dirty</div>
+            <div className="top-items-container-list">Monday syed</div>
+          </div>
+        </div>
       </Fragment>
     )
   }
